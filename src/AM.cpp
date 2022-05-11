589e05f39117e255baa03dca137c0db593cd5a1f
alejbormeg <alejbormeg@gmail.com>
2022-05-11 18:31:25 +0200

    mejoras acabadas, ya funciona todo correctamente

--- src/AM.cpp
+++ src/AM.cpp
@@ -27,58 +27,6 @@ std::vector<int> CalculaMejores(std::vector<double> vfitness, int n){
     return posiciones;
 }
 
-void ReemplazarYEvaluarAM(std::vector<std::vector<double>> & poblacion,std::vector<std::vector<double>> const & mutaciones,std::vector<std::pair<std::vector<double>,std::string>> &datos,std::vector<double> & w,double &fitness, std::vector<double> & vfitness){
-    double fitness1=0.0, minimofitness=100.0,maximofitness=0.0;
-    double tasa_clas=0.0, tasa_red_=0.0;
-    int indice_peor=0,indice_mejor=0;
-    std::vector<double> v;
-
-    //Limpiamos la población anterior
-    poblacion.clear();
-
-    //Evaluamos todos los vectores de la nueva población
-    for(int i=0; i<mutaciones.size();i++){
-        v=mutaciones[i];
-        //Calculamos el valor de fitness de v1
-        tasa_clas=LeaveOneOut(datos,v);
-        tasa_red_=tasa_red(v);
-        fitness1=funcionEvaluacion(tasa_clas,tasa_red_);
-        if(fitness1<minimofitness){
-            indice_peor=i;
-            minimofitness=fitness1;
-        }
-
-        if(fitness1>maximofitness){
-            indice_mejor=i;
-            maximofitness=fitness1;
-        }
-
-        vfitness.push_back(fitness1);
-    }
-
-    //Si el mínimo es mejor que la solución de la población anterior
-    if(minimofitness>fitness){
-        //No conservamos el mejor de la anterior   
-        poblacion=mutaciones;
-    }else{
-        //En caso contrario eliminamos el peor de mutaciones y lo reemplazamos si no está
-        /*
-        if(!Contiene(mutaciones,w)){
-            poblacion=mutaciones;
-            poblacion[indice_peor]=w;
-        }
-        else{
-            poblacion=mutaciones;
-        }
-        */
-    }
-
-    if(maximofitness>fitness){
-        w=poblacion[indice_mejor];
-        fitness=maximofitness;
-    }
-}
-
 void BusquedaLocalAM(std::vector<std::pair<std::vector<double>,std::string>> &datos,std::vector<double>&w, int &contador_evaluaciones, std::mt19937 &generator){
     std::vector<double> w_mutado=w;
     bool mejora;
@@ -126,7 +74,6 @@ void AlgoritmoMemetico(std::vector<std::pair<std::vector<double>,std::string>> &
     std::vector<std::vector<double>> poblacion,seleccion,cruce;
     int solucion;
     int evaluaciones=0;
-    double fitness = 0.0;
     int dim=datos[0].first.size();
     int t=0; //Contador generaciones 
     //Numero de cromosomas que hacen BL
@@ -134,14 +81,16 @@ void AlgoritmoMemetico(std::vector<std::pair<std::vector<double>,std::string>> &
     std::vector<double> vfitness;  // guarda los valores de fitness de los elementos de la poblacion
     std::vector<int> pos_mejores;//Guarda las posiciones de los mejores elementos, solo en caso de 0.1*N mejores
 
+    //Inicializamos la población
     poblacion=Inicializar(tam_pob,dim,gen);
-    //Evaluacion(poblacion,datos,solucion,fitness,vfitness);
+    Evaluacion(poblacion,datos,solucion,vfitness);
+    //Solución actual
     w=poblacion[solucion];
     while(evaluaciones<15000){
         Seleccion(datos,poblacion,seleccion,gen,tam_pob,vfitness);
         Cruce(seleccion,tipo,0.3,0.7,cruce,gen);
         Mutacion(cruce,0.1,gen);
-        ReemplazarYEvaluarAM(poblacion,cruce,datos,w,fitness,vfitness);
+        ReemplazarYEvaluar(poblacion,cruce,datos,w,solucion,vfitness);
         evaluaciones+=tam_pob;
         t++;
         seleccion.clear();
